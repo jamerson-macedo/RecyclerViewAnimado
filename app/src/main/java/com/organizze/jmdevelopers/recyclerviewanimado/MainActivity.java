02bc8b3c5dbe3dbbae1728db2e9b34dd18112a0a
jamerson_macedo@hotmail.com <jamerson_macedo@hotmail.com>
2019-03-25 20:58:37 -0300

    Desativando a toolbar

--- app/src/main/java/com/organizze/jmdevelopers/recyclerviewanimado/MainActivity.java
+++ app/src/main/java/com/organizze/jmdevelopers/recyclerviewanimado/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Window;
+import android.view.WindowManager;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,17 +19,32 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         recyclerView=findViewById(R.id.recyclerview);
         recyclerView.setHasFixedSize(true);
+        getSupportActionBar().hide();
+        // removendo a action bar a TOOLBAR
+
+
 
         items=new ArrayList<>();
 
-        items.add(new Item("sdnfsdnifid","nsdjnfsdjfn","mkfsfdk",R.drawable.maxresdefault));
+        items.add(new Item("NJDSFNJOFDSANDFSNJSDFAJNODFASNJODFJNOSFDJNOSDAFNOJDFSONJFSDANJASFDOJSDA","jamerson macedo","01-09-1997",R.drawable.maxresdefault));
+        items.add(new Item("bom dia","FDFGSGSDFFSDFFGGFFSD","01-09-1997",R.drawable.maxresdefault));
+        items.add(new Item("joao","ndfssdf","01-09-1997",R.drawable.maxresdefault));
+        items.add(new Item("sdnfsdnifid","nsdjnfsdjfn","01-09-1997",R.drawable.maxresdefault));
+        items.add(new Item("sdnfsdnifid","nsdjnfsdjfn","01-09-1997",R.drawable.maxresdefault));
+        items.add(new Item("sdnfsdnifid","nsdjnfsdjfn","01-09-1997",R.drawable.maxresdefault));
+        items.add(new Item("sdnfsdnifid","nsdjnfsdjfn","01-09-1997",R.drawable.maxresdefault));
+        items.add(new Item("sdnfsdnifid","nsdjnfsdjfn","01-09-1997",R.drawable.maxresdefault));
+        items.add(new Item("sdnfsdnifid","nsdjnfsdjfn","01-09-1997",R.drawable.maxresdefault));
+        items.add(new Item("sdnfsdnifid","nsdjnfsdjfn","01-09-1997",R.drawable.maxresdefault));
         // adapter
         adapter=new ItemAdapter(this,items);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
+        
     }
 }
