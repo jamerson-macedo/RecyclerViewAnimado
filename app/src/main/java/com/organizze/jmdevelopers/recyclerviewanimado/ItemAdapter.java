e4dfd4d070de98a8c6ff2ac0ec2644430dd85f53
jamerson_macedo@hotmail.com <jamerson_macedo@hotmail.com>
2019-04-04 00:32:10 -0300

    função pesquisar pronta

--- app/src/main/java/com/organizze/jmdevelopers/recyclerviewanimado/ItemAdapter.java
+++ app/src/main/java/com/organizze/jmdevelopers/recyclerviewanimado/ItemAdapter.java
@@ -3,10 +3,13 @@ package com.organizze.jmdevelopers.recyclerviewanimado;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AnimationUtils;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -17,27 +20,32 @@ import java.util.zip.Inflater;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
-public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemViewHolder> {
+// para gerar a pesquisa imlemena o metodo filterable e uma nova lista para guardar as pesquisas
+public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemViewHolder> implements Filterable {
     public Context mcontext;
     public ArrayList<Item> mlista;
+    public ArrayList<Item> mlistaFilter;
     public boolean isdark;
 
     public ItemAdapter(Context mcontext, ArrayList<Item> mlista, boolean isdark) {
         this.mcontext = mcontext;
         this.mlista = mlista;
         this.isdark = isdark;
+        this.mlistaFilter = mlista;
     }
 
     public ItemAdapter(Context mcontext, ArrayList<Item> mlista) {
         this.mcontext = mcontext;
         this.mlista = mlista;
+        this.mlistaFilter = mlista;
+
     }
 
     @NonNull
     @Override
     public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
 
-        View layout= LayoutInflater.from(mcontext).inflate(R.layout.items_view,viewGroup,false);
+        View layout = LayoutInflater.from(mcontext).inflate(R.layout.items_view, viewGroup, false);
         return new ItemViewHolder(layout);
 
     }
@@ -45,50 +53,90 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemViewHolder
     @Override
     public void onBindViewHolder(@NonNull ItemViewHolder itemViewHolder, int i) {
         // inserindo animação na imagem
-        itemViewHolder.imagem_user.setAnimation(AnimationUtils.loadAnimation(mcontext,R.anim.fade_transition));
+        itemViewHolder.imagem_user.setAnimation(AnimationUtils.loadAnimation(mcontext, R.anim.fade_transition));
         // animacao no relative layout
-        itemViewHolder.relativeLayout.setAnimation(AnimationUtils.loadAnimation(mcontext,R.anim.fades_scale_animation));
-        itemViewHolder.titulo.setText(mlista.get(i).getTitulo());
-        itemViewHolder.data.setText(mlista.get(i).getData());
-        itemViewHolder.conteudo.setText(mlista.get(i).getConteudo());
-        itemViewHolder.imagem_user.setImageResource(mlista.get(i).getUserfoto());
+        itemViewHolder.relativeLayout.setAnimation(AnimationUtils.loadAnimation(mcontext, R.anim.fades_scale_animation));
 
+        itemViewHolder.titulo.setText(mlistaFilter.get(i).getTitulo());
+        itemViewHolder.data.setText(mlistaFilter.get(i).getData());
+        itemViewHolder.conteudo.setText(mlistaFilter.get(i).getConteudo());
+        itemViewHolder.imagem_user.setImageResource(mlistaFilter.get(i).getUserfoto());
 
 
     }
 
     @Override
     public int getItemCount() {
-        return mlista.size();
+        return mlistaFilter.size();
+    }
+
+    ///// metodo do filtro
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+
+               // pegando o valor digittado aqui
+                String key = constraint.toString();
+                Log.d("key1111", key);
+                if (key.isEmpty()) {
+                    mlistaFilter = mlista;
+                }else {
+
+                    ArrayList<Item> listafiltrada=new ArrayList<>();
+                    for(Item item: mlista){
+                        if (item.getTitulo().toLowerCase().contains(key.toLowerCase())){
+                            listafiltrada.add(item);
+
+
+                        }
+
+                    }
+                    mlistaFilter= listafiltrada;
+                }
+                // reornando a pesquisa
+                FilterResults filterResults=new FilterResults();
+                filterResults.values=mlistaFilter;
+                return filterResults;
+
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+                mlistaFilter= (ArrayList<Item>) results.values;
+            notifyDataSetChanged();
+            }
+        };
     }
 
 
-    public class ItemViewHolder extends RecyclerView.ViewHolder{
+    public class ItemViewHolder extends RecyclerView.ViewHolder {
         // recuperar os ids
         CircleImageView imagem_user;
-        TextView conteudo,data,titulo;
+        TextView conteudo, data, titulo;
         RelativeLayout relativeLayout;
 
 
         public ItemViewHolder(@NonNull View itemView) {
             super(itemView);
-            relativeLayout=itemView.findViewById(R.id.relativeLayout);
-            imagem_user=itemView.findViewById(R.id.imagem_user);
-            conteudo=itemView.findViewById(R.id.conteudo);
-            data=itemView.findViewById(R.id.data);
-            titulo=itemView.findViewById(R.id.titulo);
+            relativeLayout = itemView.findViewById(R.id.relativeLayout);
+            imagem_user = itemView.findViewById(R.id.imagem_user);
+            conteudo = itemView.findViewById(R.id.conteudo);
+            data = itemView.findViewById(R.id.data);
+            titulo = itemView.findViewById(R.id.titulo);
 
             /// verificar se é preto
-            if(isdark){
+            if (isdark) {
                 setbg();
 
             }
 
 
         }
-        private void setbg(){
-            relativeLayout.setBackgroundResource(R.drawable.cardbgpreto);
 
+        private void setbg() {
+            relativeLayout.setBackgroundResource(R.drawable.cardbgpreto);
 
 
         }
--- app/src/main/java/com/organizze/jmdevelopers/recyclerviewanimado/MainActivity.java
+++ app/src/main/java/com/organizze/jmdevelopers/recyclerviewanimado/MainActivity.java
@@ -8,9 +8,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.EditText;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,101 +23,125 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<Item> items;
     private ItemAdapter adapter;
     private FloatingActionButton floatingActionButton;
-    boolean isDark=false;
+    boolean isDark = false;
     ConstraintLayout constraintLayout;
+    private EditText seach;
 
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-        recyclerView=findViewById(R.id.recyclerview);
+        recyclerView = findViewById(R.id.recyclerview);
         recyclerView.setHasFixedSize(true);
-        floatingActionButton=findViewById(R.id.fab);
-        constraintLayout=findViewById(R.id.constraint);
+        floatingActionButton = findViewById(R.id.fab);
+        constraintLayout = findViewById(R.id.constraint);
+        seach = findViewById(R.id.seach);
 
         getSupportActionBar().hide();
         // removendo a action bar a TOOLBAR
 
 
-
-        items=new ArrayList<>();
+        items = new ArrayList<>();
 
         /// carregar o thema
-        isDark=gettheme();
-        if(isDark){
+        isDark = gettheme();
+        if (isDark) {
+            seach.setBackgroundResource(R.drawable.seach_input_dark);
             constraintLayout.setBackgroundColor(getResources().getColor(R.color.preto));
 
 
+        } else {
+            seach.setBackgroundResource(R.drawable.seach_input);
 
-        }else{
             constraintLayout.setBackgroundColor(getResources().getColor(R.color.branco));
 
 
         }
 
 
-
-
-        items.add(new Item("NJDSFNJOFDSANDFSNJSDFAJNODFASNJODFJNOSFDJNOSDAFNOJDFSONJFSDANJASFDOJSDA","Jamerson Macedo","01-09-1997",R.drawable.maxresdefault));
-        items.add(new Item("bom dia","FDFGSGSDFFSDFFGGFFSD","01-09-1997",R.drawable.maxresdefault));
-        items.add(new Item("asdmfiasofmasdoifmasdoifmasdoifmasdiofmasdiofmasdiomfasdiofm","ndfssdf","01-09-1997",R.drawable.maxresdefault));
-        items.add(new Item("sdnfsdnifid","Macedo","01-09-1997",R.drawable.maxresdefault));
-        items.add(new Item("sdnfsdnifid","Arajo ","01-09-1997",R.drawable.maxresdefault));
-        items.add(new Item("sdnfsdnifid","idsoasodos sobre asidnsiad","01-09-1997",R.drawable.maxresdefault));
-        items.add(new Item("sdnfsdnifid","nsdjnfsdjfn","01-09-1997",R.drawable.maxresdefault));
-        items.add(new Item("sdnfsdnifid","nsdjnfsdjfn","01-09-1997",R.drawable.maxresdefault));
-        items.add(new Item("sdnfsdnifid","nsdjnfsdjfn","01-09-1997",R.drawable.maxresdefault));
-        items.add(new Item("sdnfsdnifid","nsdjnfsdjfn","01-09-1997",R.drawable.maxresdefault));
-        items.add(new Item("NJDSFNJOFDSANDFSNJSDFAJNODFASNJODFJNOSFDJNOSDAFNOJDFSONJFSDANJASFDOJSDA","Jamerson Macedo","01-09-1997",R.drawable.maxresdefault));
-        items.add(new Item("NJDSFNJOFDSANDFSNJSDFAJNODFASNJODFJNOSFDJNOSDAFNOJDFSONJFSDANJASFDOJSDA","Jamerson Macedo","01-09-1997",R.drawable.maxresdefault));
-        items.add(new Item("NJDSFNJOFDSANDFSNJSDFAJNODFASNJODFJNOSFDJNOSDAFNOJDFSONJFSDANJASFDOJSDA","Jamerson Macedo","01-09-1997",R.drawable.maxresdefault));
-        items.add(new Item("NJDSFNJOFDSANDFSNJSDFAJNODFASNJODFJNOSFDJNOSDAFNOJDFSONJFSDANJASFDOJSDA","Jamerson Macedo","01-09-1997",R.drawable.maxresdefault));
-        items.add(new Item("NJDSFNJOFDSANDFSNJSDFAJNODFASNJODFJNOSFDJNOSDAFNOJDFSONJFSDANJASFDOJSDA","Jamerson Macedo","01-09-1997",R.drawable.maxresdefault));
-        items.add(new Item("NJDSFNJOFDSANDFSNJSDFAJNODFASNJODFJNOSFDJNOSDAFNOJDFSONJFSDANJASFDOJSDA","Jamerson Macedo","01-09-1997",R.drawable.maxresdefault));
-        items.add(new Item("NJDSFNJOFDSANDFSNJSDFAJNODFASNJODFJNOSFDJNOSDAFNOJDFSONJFSDANJASFDOJSDA","Jamerson Macedo","01-09-1997",R.drawable.maxresdefault));
-        items.add(new Item("NJDSFNJOFDSANDFSNJSDFAJNODFASNJODFJNOSFDJNOSDAFNOJDFSONJFSDANJASFDOJSDA","Jamerson Macedo","01-09-1997",R.drawable.maxresdefault));
+        items.add(new Item("NJDSFNJOFDSANDFSNJSDFAJNODFASNJODFJNOSFDJNOSDAFNOJDFSONJFSDANJASFDOJSDA", "Jamerson Macedo", "01-09-1997", R.drawable.maxresdefault));
+        items.add(new Item("bom dia", "FDFGSGSDFFSDFFGGFFSD", "01-09-1997", R.drawable.maxresdefault));
+        items.add(new Item("asdmfiasofmasdoifmasdoifmasdoifmasdiofmasdiofmasdiomfasdiofm", "ndfssdf", "01-09-1997", R.drawable.maxresdefault));
+        items.add(new Item("sdnfsdnifid", "Macedo", "01-09-1997", R.drawable.maxresdefault));
+        items.add(new Item("sdnfsdnifid", "Arajo ", "01-09-1997", R.drawable.maxresdefault));
+        items.add(new Item("sdnfsdnifid", "idsoasodos sobre asidnsiad", "01-09-1997", R.drawable.maxresdefault));
+        items.add(new Item("sdnfsdnifid", "nsdjnfsdjfn", "01-09-1997", R.drawable.maxresdefault));
+        items.add(new Item("sdnfsdnifid", "nsdjnfsdjfn", "01-09-1997", R.drawable.maxresdefault));
+        items.add(new Item("sdnfsdnifid", "nsdjnfsdjfn", "01-09-1997", R.drawable.maxresdefault));
+        items.add(new Item("sdnfsdnifid", "nsdjnfsdjfn", "01-09-1997", R.drawable.maxresdefault));
+        items.add(new Item("NJDSFNJOFDSANDFSNJSDFAJNODFASNJODFJNOSFDJNOSDAFNOJDFSONJFSDANJASFDOJSDA", "Jamerson Macedo", "01-09-1997", R.drawable.maxresdefault));
+        items.add(new Item("NJDSFNJOFDSANDFSNJSDFAJNODFASNJODFJNOSFDJNOSDAFNOJDFSONJFSDANJASFDOJSDA", "Jamerson Macedo", "01-09-1997", R.drawable.maxresdefault));
+        items.add(new Item("NJDSFNJOFDSANDFSNJSDFAJNODFASNJODFJNOSFDJNOSDAFNOJDFSONJFSDANJASFDOJSDA", "Jamerson Macedo", "01-09-1997", R.drawable.maxresdefault));
+        items.add(new Item("NJDSFNJOFDSANDFSNJSDFAJNODFASNJODFJNOSFDJNOSDAFNOJDFSONJFSDANJASFDOJSDA", "Jamerson Macedo", "01-09-1997", R.drawable.maxresdefault));
+        items.add(new Item("NJDSFNJOFDSANDFSNJSDFAJNODFASNJODFJNOSFDJNOSDAFNOJDFSONJFSDANJASFDOJSDA", "Jamerson Macedo", "01-09-1997", R.drawable.maxresdefault));
+        items.add(new Item("NJDSFNJOFDSANDFSNJSDFAJNODFASNJODFJNOSFDJNOSDAFNOJDFSONJFSDANJASFDOJSDA", "Jamerson Macedo", "01-09-1997", R.drawable.maxresdefault));
+        items.add(new Item("NJDSFNJOFDSANDFSNJSDFAJNODFASNJODFJNOSFDJNOSDAFNOJDFSONJFSDANJASFDOJSDA", "Jamerson Macedo", "01-09-1997", R.drawable.maxresdefault));
+        items.add(new Item("NJDSFNJOFDSANDFSNJSDFAJNODFASNJODFJNOSFDJNOSDAFNOJDFSONJFSDANJASFDOJSDA", "Jamerson Macedo", "01-09-1997", R.drawable.maxresdefault));
 
         // adapter
-        adapter=new ItemAdapter(this,items,isDark);
+        adapter = new ItemAdapter(this, items, isDark);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                isDark=!isDark;
-                if(isDark){
+                isDark = !isDark;
+                if (isDark) {
                     constraintLayout.setBackgroundColor(getResources().getColor(R.color.preto));
+                    seach.setBackgroundResource(R.drawable.seach_input_dark);
 
-                }else {
+
+                } else {
+                    seach.setBackgroundResource(R.drawable.seach_input);
 
                     constraintLayout.setBackgroundColor(getResources().getColor(R.color.branco));
 
+
                 }
-                 adapter=new ItemAdapter(getApplicationContext(),items,isDark);
+                adapter = new ItemAdapter(getApplicationContext(), items, isDark);
                 recyclerView.setAdapter(adapter);
                 salvartema(isDark);
 
             }
         });
+
+        seach.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                adapter.getFilter().filter(s);
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+
     }
 
+
     private void salvartema(boolean isDark) {
-        SharedPreferences sharedPreferences=getApplicationContext().getSharedPreferences("mypref",MODE_PRIVATE);
-        SharedPreferences.Editor editor=sharedPreferences.edit();
-        editor.putBoolean("isdark",isDark);
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("mypref", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean("isdark", isDark);
         editor.commit();
 
     }
-    private boolean gettheme(){
-        SharedPreferences sharedPreferences=getApplicationContext().getSharedPreferences("mypref",MODE_PRIVATE);
-        boolean isdark=sharedPreferences.getBoolean("isdark", false);
+
+    private boolean gettheme() {
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("mypref", MODE_PRIVATE);
+        boolean isdark = sharedPreferences.getBoolean("isdark", false);
         return isdark;
-
-
-
 
 
     }
